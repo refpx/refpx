352514c7dc3796d5f4b76ccfafc2c8e3fb09d8a1
Fredy Romero <romeroespinoza.fp@gmail.com>
2023-03-29 17:39:42 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://www.example.com",
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+      entryLimit: 10000,
+    }),
+  ],
+});
